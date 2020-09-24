d21c67e59a8c3178587d7d6f7c482fd482ad22a3
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-09-24 14:17:58 +0200

    B3 - avg can be decimal

--- B3/B3.java
+++ B3/B3.java
@@ -3,12 +3,12 @@ public class B3 {
 
 	public static void main(String[] args) {
 		
-		int[] numbers = {1,2,6,7,8,9,15,22,45,72,89};
-		int total= 0;
+		int[] numbers = {1,1,2,6,7,8,9,15,22,45,72,89,94};
+		double total= 0;
 		for (int index: numbers) { 
 			total += index;
 		}
-		int avg = total/numbers.length;
+		double avg = total/numbers.length;
 			
 		System.out.println("The average for numbers is " + avg);
 	}	
