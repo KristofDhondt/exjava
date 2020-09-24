4dc2767af03caabfeb7db33d1b874834d97f2cb6
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-09-24 10:17:21 +0200

    add exercise A1

---
+++ A1/A1.java
@@ -0,0 +1,12 @@
+public class A1{
+
+	public static void main(String[] args) {
+		String courseTitle = "Java";
+		int numberOfDays = 3;
+		double priceDay = 245.23;
+		Boolean knowledgeRequired = true;
+		
+		System.out.println ("Course name " +courseTitle + " during " + numberOfDays + " costs " + priceDay + " a day. " + "knowledge needed : " +knowledgeRequired);
+	}	
+			
+}
